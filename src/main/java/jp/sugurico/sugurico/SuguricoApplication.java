03fed63ca4a83111fc0adaa5e62caf88a98a0dc5
fukuda-hiroki <2311090@i-seifu.jp>
2025-06-12 16:36:12 +0900

    dd

---
+++ src/main/java/jp/sugurico/sugurico/SuguricoApplication.java
@@ -0,0 +1,13 @@
+package jp.sugurico.sugurico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuguricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuguricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/sugurico/sugurico/SuguricoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.sugurico.sugurico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuguricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
